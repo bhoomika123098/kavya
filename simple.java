e7b2734310aa2a4cdecd9788958d4e7178a2ecc3
kavya <kavya123@gmail.com>
2024-07-03 02:24:45 -0400

    change2

--- simple.java
+++ simple.java
@@ -2,6 +2,7 @@ public class simple java
 {
 	public static void main(String[]args)
 	{
-		System.out.println("we3lcome to tumkuru");
+		System.out.println("welcome to tumkuru");
+		System.out.println("java");
 	}
 }
